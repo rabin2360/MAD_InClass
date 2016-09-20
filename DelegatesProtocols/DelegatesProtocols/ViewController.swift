cb238b14a9d5eae2b28439bfba59828800e1278b
rabin2360 <rana_rabin@yahoo.com>
2016-09-20 12:41:49 -0600

    Adding a new project

---
+++ DelegatesProtocols/DelegatesProtocols/ViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ViewController.swift
+//  DelegatesProtocols
+//
+//  Created by Rabin Ranabhat on 9/20/16.
+//  Copyright © 2016 Rabin Ranabhat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    //outlets for the application
+    @IBOutlet weak var checkAmount: UITextField!
+    @IBOutlet weak var tipPercent: UITextField!
+    @IBOutlet weak var people: UITextField!
+    @IBOutlet weak var tipDue: UITextField!
+    @IBOutlet weak var totalDue: UILabel!
+    @IBOutlet weak var totalDuePerPerson: UILabel!
+    
+    //pressing the done button makes the key board go away
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        updateTipTotals()
+    }
+    
+    override func viewDidLoad() {
+        //setting the delegates for all the text buttons
+        checkAmount.delegate = self
+        tipPercent.delegate = self
+        people.delegate = self
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func updateTipTotals()
+    {
+    
+        var amount: Float
+        var pct: Float
+        
+        if checkAmount.text!.isEmpty
+        {
+            amount = 0.0
+        }
+        else
+        {
+            amount = Float(checkAmount.text!)!
+        }
+        
+        if tipPercent.text!.isEmpty
+        {
+            pct = 0.0
+        }
+        else
+        {
+            pct = Float(tipPercent.text!)!/100
+        }
+        
+        let numberOfPeople = Int(people.text!)
+        let tip = amount*pct
+        let total = amount + tip
+        var personTotal : Float = 0.0
+        
+        if numberOfPeople != nil
+        {
+            if numberOfPeople > 0
+            {
+                personTotal = total / Float(numberOfPeople!)
+            }
+        }
+        
+        //format results as currency
+        let currentFormatter = NSNumberFormatter()
+        currentFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+        tipDue.text = currentFormatter.stringFromNumber(tip)
+        totalDue.text = currentFormatter.stringFromNumber(total)
+        totalDuePerPerson.text = currentFormatter.stringFromNumber(personTotal)
+
+}
+    
+}

0c1170a99e899ac5aabf051b6d9595b8c5fa7633
Rabin Ranabhat <rabinranabhat@Rabins-Mac-mini.local>
2016-09-24 19:26:14 -0600

    Adding adaptive layout to delegate and protocols project

--- buttonClicks/buttonClicks/ViewController.swift
+++ buttonClicks/buttonClicks/ViewController.swift
@@ -12,18 +12,23 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var pictureDisplay: UIImageView!
     
+    @IBOutlet weak var labelDisplayer: UILabel!
+    
     @IBAction func paintingsPressed(sender: UIButton) {
         pictureDisplay.image = UIImage(named: "monalisa.png")
+        labelDisplayer.text = "MonaLisa"
     }
     
     @IBAction func drawingPressed(sender: UIButton) {
         pictureDisplay.image = UIImage(named: "anatomy.png")
+        labelDisplayer.text = "Anatomy picture"
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        labelDisplayer.text = "Leonardo Da Vinci"
     }
 
     override func didReceiveMemoryWarning() {

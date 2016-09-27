99b34c6594b9a0f603a74582331bcc05b5443e4b
rabin2360 <rana_rabin@yahoo.com>
2016-09-27 12:53:44 -0600

    Adding multiple view

---
+++ FavoritesApp/FavoritesApp/Scene2ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Scene2ViewController.swift
+//  FavoritesApp
+//
+//  Created by Rabin Ranabhat on 9/27/16.
+//  Copyright © 2016 Rabin Ranabhat. All rights reserved.
+//
+
+import UIKit
+
+class Scene2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FavoritesApp/FavoritesApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FavoritesApp
+//
+//  Created by Rabin Ranabhat on 9/27/16.
+//  Copyright © 2016 Rabin Ranabhat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

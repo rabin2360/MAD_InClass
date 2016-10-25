1a1f78d9b958fb1eb76f4d707041dc19cd5d438e
rabin2360 <rana_rabin@yahoo.com>
2016-10-25 12:45:40 -0600

    Adding StudentForm Project1

--- Project1/app/src/main/java/com/example/rara5776/project1/MainActivity.java
+++ Project1/app/src/main/java/com/example/rara5776/project1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.rara5776.project1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +17,34 @@ public class MainActivity extends AppCompatActivity {
     TextView booLabel;
     EditText booTextField;
     ImageView image;
+    TextView charRemaining;
+
+    private final int MAXNUMBER = 20;
+
+    private final TextWatcher watchTextEdits = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            String displayText;
+            if(charSequence.length() < MAXNUMBER)
+                displayText = String.valueOf("Current length is: "+charSequence.length());
+            else
+                displayText = String.valueOf("Max length reached: "+charSequence.length());
+
+            charRemaining.setText(String.valueOf(displayText));
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
 
     public void buttonPressed(View view)
     {
@@ -32,6 +62,9 @@ public class MainActivity extends AppCompatActivity {
         booLabel = (TextView) findViewById(R.id.message);
         booTextField = (EditText) findViewById(R.id.enterBooText);
         image = (ImageView) findViewById(R.id.imageView);
+        charRemaining = (TextView) findViewById(R.id.charRemaining);
 
+        //adding listener for the EditText
+        booTextField.addTextChangedListener(watchTextEdits);
     }
 }
---
+++ StudentForm/app/src/main/java/com/example/rara5776/studentform/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.rara5776.studentform;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //spinner
+        Spinner studentGrades = (Spinner)findViewById(R.id.spinner);
+        String studentGrade = String.valueOf(studentGrades.getSelectedItem());
+    }
+}

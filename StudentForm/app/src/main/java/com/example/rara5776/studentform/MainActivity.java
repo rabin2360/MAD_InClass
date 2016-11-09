bee696697504af9b04e1873bb6bc669de8c9e2a5
rabin2360 <rana_rabin@yahoo.com>
2016-11-08 21:57:14 -0700

    Adding the student form project with the changes

--- StudentForm/app/src/main/java/com/example/rara5776/studentform/MainActivity.java
+++ StudentForm/app/src/main/java/com/example/rara5776/studentform/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.rara5776.studentform;
 
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.view.View;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,17 +22,44 @@ public class MainActivity extends AppCompatActivity {
     RadioGroup radioGroupVar;
     CheckBox checkBox1;
     CheckBox checkBox2;
+    ImageView image;
+    TextView display;
 
     //view variables
     String studentGradeStr;
 
     public void submitButtonPressed(View view)
     {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(studentName.getText().toString());
+        sb.append(", who is ");
+
         //submit button has been pressed
         Log.v("Name: ", studentName.getText().toString());
 
         studentGradeStr = String.valueOf(studentGrades.getSelectedItem());
+
+        if(studentGradeStr.equals("Freshman"))
+        {
+            image.setImageResource(R.drawable.freshman);
+        }
+        else if(studentGradeStr.equals("Sophomore"))
+        {
+            image.setImageResource(R.drawable.sophomore);
+        }
+        else if(studentGradeStr.equals("Junior"))
+        {
+            image.setImageResource(R.drawable.junior);
+        }
+        else
+        {
+            image.setImageResource(R.drawable.senior);
+        }
+
         Log.v("Student Grade: ", studentGradeStr);
+        sb.append(studentGradeStr);
+        sb.append(" and lives ");
 
         //processing the toggle button
         String toggleString;
@@ -46,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Log.v("Toggle: ", toggleString);
+        sb.append(toggleString);
 
         //switches
         String switchString = "";
@@ -53,14 +82,16 @@ public class MainActivity extends AppCompatActivity {
 
         if(studentSwitchBool)
         {
-            switchString = "Switch is on";
+            switchString = ", meditates";
         }
         else
         {
-            switchString = "Switch is off";
+            switchString = ", does not meditate";
         }
 
         Log.v("Switch:", switchString);
+        sb.append(switchString);
+        sb.append(", votes ");
 
         //radio buttons group
         String radioButtonType = "";
@@ -69,35 +100,48 @@ public class MainActivity extends AppCompatActivity {
         switch(radioButtonId)
         {
             case -1:
-                radioButtonType = "Not defined";
+                radioButtonType = "Not defined ";
                 break;
 
             case R.id.firstButton:
-                radioButtonType = "firstButton";
+                radioButtonType = "Republican ";
                 break;
 
             case R.id.secondButton:
-                radioButtonType = "secondButton";
+                radioButtonType = "Democrat ";
                 break;
 
             default:
-                radioButtonType = "error";
+                radioButtonType = "error ";
         }
 
         Log.v("Radio Button Type:", radioButtonType);
+        sb.append(radioButtonType);
+        sb.append("and plays ");
 
         boolean check1 = checkBox1.isChecked();
         boolean check2 = checkBox2.isChecked();
 
-        if(check1)
+        if(check1 && check2)
         {
-            Log.v("Check1:", "is enabled" );
+            sb.append(checkBox1.getText().toString());
+            sb.append(" and ");
+            sb.append(checkBox2.getText().toString());
+
+        }
+        else if(check2)
+        {
+            sb.append(checkBox2.getText().toString());
+        }
+        else if(check1)
+        {
+            sb.append(checkBox1.getText().toString());
         }
 
-        if(check2)
-        {
-            Log.v("Check2: ", "is enabled");
-        }
+        sb.append(".");
+
+        Log.v("FINAL", sb.toString());
+        display.setText(sb.toString());
     }
 
     @Override
@@ -125,6 +169,13 @@ public class MainActivity extends AppCompatActivity {
 
         //check box 2
         checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
+
+        //imageview
+        image = (ImageView) findViewById(R.id.imageView);
+        image.setImageResource(R.drawable.freshman);
+
+        //display
+        display = (TextView) findViewById(R.id.displayLabel);
     }
 
 }

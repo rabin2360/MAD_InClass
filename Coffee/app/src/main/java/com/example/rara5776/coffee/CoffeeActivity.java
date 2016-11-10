55b1e482768c841ae9dc48f636cc2acc4963923a
rabin2360 <rana_rabin@yahoo.com>
2016-11-10 12:48:42 -0700

    Adding the on click button listener

--- Coffee/app/src/main/java/com/example/rara5776/coffee/CoffeeActivity.java
+++ Coffee/app/src/main/java/com/example/rara5776/coffee/CoffeeActivity.java
@@ -3,6 +3,7 @@ package com.example.rara5776.coffee;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class CoffeeActivity extends AppCompatActivity {
@@ -13,6 +14,19 @@ public class CoffeeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coffee);
+
+        //button
+        final Button button = (Button) findViewById(R.id.submitButton);
+
+        View.OnClickListener onclick = new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                findCoffee(view);
+            }
+        };
+
+        button.setOnClickListener(onclick);
     }
 
     public void findCoffee(View view)
